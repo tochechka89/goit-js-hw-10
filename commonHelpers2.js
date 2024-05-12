a03d57dcdcbe2540914846ff2f15740599d310d9
tochechka89 <152875004+tochechka89@users.noreply.github.com>
2024-05-12 20:46:38 +0000

    Deploying to gh-pages from @ tochechka89/goit-js-hw-10@6af16e33d3607e8cc68c4908e1f4ad723d896382 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{i as r}from"./assets/vendor-77e16229.js";function n(e,s){return new Promise((i,t)=>{setTimeout(()=>{s==="fulfilled"?i(e):t(e)},e)})}document.querySelector(".form").addEventListener("submit",function(e){e.preventDefault();const s=parseInt(this.elements.delay.value),i=this.elements.state.value;n(s,i).then(t=>{r.success({title:"Success",message:`✅ Fulfilled promise in ${t}ms`})}).catch(t=>{r.error({title:"Error",message:`❌ Rejected promise in ${t}ms`})})});
+//# sourceMappingURL=commonHelpers2.js.map
